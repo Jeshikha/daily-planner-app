8a82e1ccc15e720b17bd350fc07fc9a00d1739f7
Jeshikha <138247355+Jeshikha@users.noreply.github.com>
2023-09-08 19:04:07 +0200

    javascript update with day js and jquery

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    // Get the current date using Day.js
+    var currentDate = dayjs().format('dddd, MMMM D');
+
+    // Display the current date in the header
+    $('#currentDay').text(currentDate);
+
+    // Function to update time block styles
+    function updateTimeBlocks() {
+        var currentTime = dayjs().format('H');
+
+        $('.time-block').each(function () {
+            var blockTime = parseInt($(this).attr('id').split('-')[1]);
+
+            if (blockTime < currentTime) {
+                $(this).addClass('past').removeClass('present future');
+            } else if (blockTime == currentTime) {
+                $(this).addClass('present').removeClass('past future');
+            } else {
+                $(this).addClass('future').removeClass('past present');
+            }
+        });
+    }
+
+    // Load saved appointments from local storage
+    function loadAppointments() {
+        $('.time-block').each(function () {
+            var blockID = $(this).attr('id');
+            var appointment = localStorage.getItem(blockID);
+
+            if (appointment) {
+                $(this).find('.description').val(appointment);
+            }
+        });
+    }
+
+    // Initialize the scheduler
+    updateTimeBlocks();
+    loadAppointments();
+
+    // Handle save button clicks
+    $('.saveBtn').on('click', function () {
+        var blockID = $(this).parent().attr('id');
+        var appointment = $(this).siblings('.description').val();
+
+        if (appointment) {
+            localStorage.setItem(blockID, appointment);
+            $('#notify').addClass('show');
+            setTimeout(function () {
+                $('#notify').removeClass('show');
+            }, 3000);
+        }
+    });
+});
+
+
+
+
+
+
